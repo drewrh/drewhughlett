322b7a24e6a2955e51c09028a57970f03bc25cc0
arhughle <arhughle@ncsu.edu>
2022-06-17 14:53:19 -0400

    Test

--- drew-hughlett-site/src/App.js
+++ drew-hughlett-site/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a test
+          This is a test part 2
         </p>
         <a
           className="App-link"
