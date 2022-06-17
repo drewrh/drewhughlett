79d2addf3758d15df5e5a85b3a83f6e103e8645c
arhughle <arhughle@ncsu.edu>
2022-06-17 14:31:57 -0400

    Test

---
+++ script.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+TARGET="/home/drew/drewhughlett"
+GIT_DIR="/home/drew/drewhughlett.git"
+BRANCH="main"
+CONTAINER_NAME="drewhughlett"
+IMAGE_NAME="home"
+
+while read oldrev newrev ref
+do
+	# only checking out the main (or whatever branch you would like to deploy)
+	if [ "$ref" = "refs/heads/$BRANCH" ];
+	then
+		filelist=$(git diff-tree --no-commit-id --name-only -r $newrev)
+		echo "Ref $ref received. Deploying ${BRANCH} branch to production..."
+		git --work-tree=$TARGET --git-dir=$GIT_DIR checkout -f $BRANCH
+		cd $TARGET
+    echo "Building new Docker image"
+		docker build -f Dockerfile -t $IMAGE_NAME .
+		echo "Redeploying UI..."
+		docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+    docker run --rm --name=$CONTAINER_NAME -p 80:80 $IMAGE_NAME
+	else
+		echo "Ref $ref received. Doing nothing: only the ${BRANCH} branch may be deployed on this server."
+	fi
+done
