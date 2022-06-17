eb5cc6de7d03af9a705ed0b5f59f208e4140994f
arhughle <arhughle@ncsu.edu>
2022-06-17 19:03:45 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing image pruning pt 5
+          Testing image pruning pt 6
         </p>
         <a
           className="App-link"
