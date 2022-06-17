c51abff977c0efae0f514deee1e74526787e0e48
arhughle <arhughle@ncsu.edu>
2022-06-17 16:18:18 -0400

    Test

--- drew-hughlett-site/src/App.js
+++ drew-hughlett-site/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing image pruning pt 3
+          Testing image pruning pt 4
         </p>
         <a
           className="App-link"
