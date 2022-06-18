6b7dd91b1d0097ed71493b176bc80d9af85bcc56
arhughle <arhughle@ncsu.edu>
2022-06-18 12:16:34 -0400

    Removed python hook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing new python post-receive hook pt 99
+          Testing new python post-receive hook finale
         </p>
         <a
           className="App-link"
