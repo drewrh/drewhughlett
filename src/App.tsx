fa69591c9af69e47d5b7dd5e2d7bfb8e94c48a23
arhughle <arhughle@ncsu.edu>
2022-06-18 13:10:30 -0400

    Added more TS

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <p>Under construction!</p>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/-ailhp8kRzE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <a target={"_blank"} href='https://github.com/drewrh/drewhughlett' rel="noreferrer">Source code</a>
       </header>
     </div>
   );
