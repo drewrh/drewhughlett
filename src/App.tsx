dacf323f4cef112a517e1dee364bfc14441c588e
arhughle <arhughle@ncsu.edu>
2022-06-18 12:54:41 -0400

    Moved to TS and added linter

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Under construction!</p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/-ailhp8kRzE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
