d5bd04a3cb208564d1167ce4845901d1242349c7
arhughle <arhughle@ncsu.edu>
2022-06-18 11:59:22 -0400

    Test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing new python post-receive hook pt 5
+          Testing new python post-receive hook pt 4
         </p>
         <a
           className="App-link"
