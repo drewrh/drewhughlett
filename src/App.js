3edfb9e9a7084683a6da1bea62e93356544a46e0
arhughle <arhughle@ncsu.edu>
2022-06-18 12:39:52 -0400

    Began removing React clutter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing new python post-receive hook finale
+          Under construction!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/-ailhp8kRzE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </header>
     </div>
   );
